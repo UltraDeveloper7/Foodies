c8c3e0da3ff08b9a8f6009b91a9ab8a2296d39cd
UltraDeveloper7 <konstantinostoutounas7@outlook.com.gr>
2024-05-24 14:08:24 +0300

    Configured session secret environment variable

--- app-setup/app-setup-session.mjs
+++ app-setup/app-setup-session.mjs
@@ -1,14 +1,17 @@
 import session from 'express-session';
 import { randomBytes } from 'crypto';
 
-// Δημιουργία ισχυρού μυστικού κωδικού
-const secretKey = randomBytes(64).toString('hex');
+// Generate a strong secret key if not provided in the environment
+const secretKey = process.env.SESSION_SECRET || randomBytes(64).toString('hex');
 
 const foodiesSession = session({
-    secret: secretKey, 
+    secret: secretKey,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true } // Note: set to true in production with HTTPS
+    cookie: {
+        secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+        maxAge: 1000 * 60 * 60 * 24 // 1 day
+    }
 });
 
 export default foodiesSession;
